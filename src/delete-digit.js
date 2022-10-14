6c93efdbf404e041edde41be2dfef57f6e1a2522
Hanna Stsefanovich <annet.stefanovich@gmail.com>
2022-10-14 22:00:35 +0300

    feat: done 7-9 tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let s = String(n);
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0) {
+      arr.push(s.slice(1))
+    }
+    else {
+      arr.push(s.slice(0, i) + s.slice(i + 1))
+    }
+  }
+  arr.sort((a, b) => b - a);
+  return Number(arr[0]);
 }
+
 
 module.exports = {
   deleteDigit
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getDNSStats(domains) {
+  let arr = [];
+  domains.forEach(item => {
+arr_small = item.split('.').reverse()
+console.log(arr_small)
+  arr.push(arr_small.find(el => el[0]));
+ 
+   if (arr_small.length > 2) {
+    arr.push(arr_small.slice(0,2).join('.'));
+       console.log(arr)
+   }
+  
+   if (!arr.includes(arr_small.join('.'))) {
+    arr.push(arr_small.join('.'));}
+ })
+ arr = arr.map(item => '.' + item)
+ let new_arr = [...new Set(arr)]
+ let result = {};
+ 
+ for (let item of new_arr) {
+     result[item] = arr.filter(el=> el===item).length;
+ }
+ 
+    console.log(result)
+ return result
 }
 
 module.exports = {
