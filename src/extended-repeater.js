0d1d164240b6cb51228f381601d481460b653d50
Hanna Stsefanovich <annet.stefanovich@gmail.com>
2022-10-15 19:12:46 +0300

    feat: done 10-12 tasks (10+-)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  let new_str = String(str);
+  if ('addition' in options) {
+if (options.additionRepeatTimes>1) {
+       for (let i = 1; i<options.additionRepeatTimes; i++) {
+    new_str = new_str+options.addition+(options.additionSeparator || '|')
+  }
+  new_str = new_str+options.addition;
+} else {
+ new_str = new_str+options.addition;
+ }
+}
+ let arr = [new_str];
+if ('repeatTimes' in options) {
+    
+if (options.repeatTimes>1) {
+      while (arr.length<options.repeatTimes) {arr.push(new_str)};
+  for (let i = 1; i<arr.length; i++) {
+    arr[i] = (options.separator || '+')+arr[i];}
+
+} }
+    
+  new_str = arr.join('');
+console.log(arr)
+
+console.log(new_str)
+
+return  new_str;
+
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  str: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.str.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (value === 'undefined') this.str.push('( )~~');
+    else this.str.push(value+'~~');
+return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof position !== 'number' || !Number.isInteger(position) || position <= 0 || position-1>this.str.length)
+    throw new Error('You can\'t remove incorrect link!')
+    else {this.str.splice(position-1, 1); 
+    return this;
+  } 
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.str.reverse();
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   return this.str.join('')
   }
 };
 
